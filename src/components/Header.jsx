5cd989286bbe2c3656a77988fee405b309e3a0b5
jayperkins150 <jayperkins150@yahoo.co.uk>
2025-12-07 19:24:14 +0000

    Add Header component with logo and text props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import logo from '../assets/logo.jpg';
+import './Header.css'
 
+export const Header = ({ title = "Meet your Financial Investmate", subtitle = "The smart investment tool" }) => {
+  return (
+    <header id="header" className="header">
+      <img src={logo} alt="Company Logo" className="header__logo" />
+      <br />
+      <div className="header__text">
+        <h2 className="header__title">{title}</h2>
+        {subtitle && <h3 className="header__subtitle">{subtitle}</h3>}
+      </div>
+    </header>
+  )
+}
